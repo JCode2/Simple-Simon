ea39c17188f9d53b92b7398f2480f9cca3e6da6e
josh <josh.fierrosga@gmail.com>
2016-11-02 14:28:02 -0500

    created an association in JS. Did this by creating new variable 'colors' with an array of all color names addocited with 'color = ...math.random...'  Once that was done fixed the match function I created by adding '.toString' to the end of the variables.

--- simon.js
+++ simon.js
@@ -5,56 +5,54 @@ var userpattern = [];
 
 
 function round() {
-    console.log(animate(sequence));
-    var color = Math.floor(Math.random() * 4) + 1;
-    console.log(color);
-    sequence.push(color);
-    function animate(sequence) {
-        var i = 0;
-        var interval = setInterval(function () {
-            flash(sequence[i]);
-            i++;
-            if (i == sequence.length) {
-                clearInterval(interval);
-            }
-        }, 500);
+    var colors = ["blue", "red", "gold", "pink"];
+    var color = Math.floor(Math.random() * 4);
 
-        console.log('WORK DAMN YOU')
-    }
+    animate(sequence);
+    sequence.push(colors[color]);
+}
+
+function animate(sequence) {
+    var i = 0;
+    var interval = setInterval(function () {
+        flash(sequence[i]);
+        i++;
+        if (i == sequence.length) {
+            clearInterval(interval);
+        }
+    }, 500);
+
+    console.log('WORK DAMN YOU')
 }
 
 function flash(square) {
-    $('[data-tile=' + square + ']').addClass('squareflash');
+    $('[data-tile="' + square + '"]').addClass('squareflash');
     window.setTimeout(function () {
-        $('[data-tile=' + square + ']').removeClass('squareflash');
+        $('[data-tile="' + square + '"]').removeClass('squareflash');
     }, 500);
 }
 
 $('.square').click(function () {
     $(this)
         .animate({"opacity": "1"})
-        .animate({"opacity": ".5"})
-    ;
+        .animate({"opacity": ".5"});
+
+    userpattern.push($(this).attr('id'));
+
+    match();
+
     console.log('it works');
 });
 
-function userInput() {
-
-    $('.square').click(function () {
-        userpattern.push($(this).attr('id'));
-        console.log(userpattern);
-    });
-}
-userInput();
-
 function match() {
-    if (userpattern = sequence) {
 
+    if (userpattern.toString() != sequence.toString()) {
+        alert('YOU LOSE!')
     }
 
     else {
 
-        alert('YOU LOSE!')
+        alert('That is correct. Click ok to continue')
 
     }
 }
