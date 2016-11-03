952cc697860ce204b737ac19223d918dd66b6073
josh <josh.fierrosga@gmail.com>
2016-11-03 14:43:37 -0500

    added 'userpattern = []' in order to empty user input array so the user can click the proper sequence.
    Removed animation in css class 'squareflash'

--- simon.js
+++ simon.js
@@ -2,15 +2,16 @@
 
 var sequence = [];      //empty arrays to push to //
 var userpattern = []; //**//\
-//var i;
 
 
 function round() {
     var colors = ["blue", "red", "gold", "pink"]; //an array of colors that will be associated with //
-    var color = Math.floor(Math.random() * 4);  //creates a random number between 0 - 4 //
+    var color = Math.floor(Math.random() * 3);  //creates a random number between 0 - 3 //
 
-    animate(sequence);
+    userpattern = [];
     sequence.push(colors[color]);       //associates the random number with the color number in the color array //
+    animate(sequence);
+    console.log(sequence);
 }
 
 function animate(sequence) {
@@ -22,8 +23,8 @@ function animate(sequence) {
             clearInterval(interval);
         }
     }, 300);
-    console.log((sequence));
-    console.log('WORK DAMN YOU')
+    console.log(sequence);
+    console.log('WORK DAMN YOU');
 }
 
 //function that adds and removes class squareflash to show the sequence//
@@ -32,14 +33,14 @@ function flash(square) {
     $('[data-tile="' + square + '"]').addClass('squareflash');
     window.setTimeout(function () {
         $('[data-tile="' + square + '"]').removeClass('squareflash');
-    }, 300);
+    }, 500);
 }
 
 //When you click a tile opacity changes, also pushes clicks to an array and compares it to the sequence array//
 
 $('.square').click(function () {
     $(this)
-        .animate({"opacity": "1"})
+        .animate({"opacity": "1.25"})
         .animate({"opacity": ".5"});
 
     userpattern.push($(this).attr('id'));
@@ -52,16 +53,31 @@ $('.square').click(function () {
 //Checks if userpattern array equals sequence array//
 
 function match() {
+    console.log(userpattern);
+    if (userpattern.length == sequence.length) {
 
-    if (userpattern.toString() != sequence.toString()) {  //not comparing the way i want
-        alert('YOU LOSE!')
-    }
 
-    else {
+        if (userpattern.toString() != sequence.toString()) {  //not comparing the way i want
+            alert('YOU LOSE!');
+            document.getElementById('start').innerHTML = "Start Over"
+        }
 
-        alert('That is correct. Click ok to continue');
-        round();
-        document.getElementById('round').innerHTML = "<h3>Round " + parseInt(sequence.length) + "</h3>";
-        console.log(parseInt(sequence))
+        else {
+
+            //alert('That is correct. Click ok to continue');
+
+            round();
+            document.getElementById('round').innerHTML = "<h3>Round " + parseInt(sequence.length) + "</h3>";
+            console.log(sequence);
+        }
     }
 }
+
+function initialround() {
+    $('#start').click(function () {
+        document.getElementById('round').innerHTML = "<h3>Round 1 </h3>";
+    })
+
+
+}
+initialround();
