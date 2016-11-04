08d6eacbae8c033a161a4de93610d6f86abd552e
josh <josh.fierrosga@gmail.com>
2016-11-04 09:45:48 -0500

    Fixed animation so that the sequence kept showing after round 3.

--- simon.js
+++ simon.js
@@ -8,13 +8,14 @@ function round() {
     var colors = ["blue", "red", "gold", "pink"]; //an array of colors that will be associated with //
     var color = Math.floor(Math.random() * 3);  //creates a random number between 0 - 3 //
 
-    userpattern = [];
+    userpattern = [];       //clears user input so they can start the sequence from the beginning everytime
     sequence.push(colors[color]);       //associates the random number with the color number in the color array //
     animate(sequence);
     console.log(sequence);
 }
 
 function animate(sequence) {
+    console.info(sequence);
     var i = 0;
     var interval = setInterval(function () {
         flash(sequence[i]);
@@ -22,18 +23,15 @@ function animate(sequence) {
         if (i == sequence.length) {
             clearInterval(interval);
         }
-    }, 300);
-    console.log(sequence);
-    console.log('WORK DAMN YOU');
+    }, 600);
 }
 
 //function that adds and removes class squareflash to show the sequence//
 
 function flash(square) {
-    $('[data-tile="' + square + '"]').addClass('squareflash');
-    window.setTimeout(function () {
-        $('[data-tile="' + square + '"]').removeClass('squareflash');
-    }, 500);
+    $('[data-tile="' + square + '"]')
+        .animate({"opacity": "1.25"})
+        .animate({"opacity": ".5"});
 }
 
 //When you click a tile opacity changes, also pushes clicks to an array and compares it to the sequence array//
@@ -46,31 +44,26 @@ $('.square').click(function () {
     userpattern.push($(this).attr('id'));
 
     match();
-
-    console.log('it works');
 });
 
 //Checks if userpattern array equals sequence array//
 
 function match() {
-    console.log(userpattern);
+
+    if (userpattern.toString() != sequence.slice(0, userpattern.length).toString()) {
+        alert('YOU LOSE!');
+        document.getElementById('start').innerHTML = "Start Over";
+        return;
+
+    }
+
     if (userpattern.length == sequence.length) {
 
-
-        if (userpattern.toString() != sequence.toString()) {  //not comparing the way i want
-            alert('YOU LOSE!');
-            document.getElementById('start').innerHTML = "Start Over"
-        }
-
-        else {
-
             //alert('That is correct. Click ok to continue');
-
             round();
             document.getElementById('round').innerHTML = "<h3>Round " + parseInt(sequence.length) + "</h3>";
-            console.log(sequence);
-        }
     }
+
 }
 
 function initialround() {
